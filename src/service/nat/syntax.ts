bb0c02a21d7821648fc1eac62d81b13cae5f36dc
Alex Shilen <ashilen1@jh.edu>
2025-06-05 13:18:39 -0400

    refactor: use @nat-lang/natvs grammar

--- src/service/nat/syntax.ts
+++ src/service/nat/syntax.ts
@@ -1,5 +1,5 @@
 
-import natGrammar from './grammar.json' with { type: 'json' };
+import natGrammar from "@nat-lang/natvs/syntaxes/nat.tmLanguage.json" with { type: 'json' };
 
 import { shikiToMonaco } from '@shikijs/monaco';
 import * as monaco from 'monaco-editor';
