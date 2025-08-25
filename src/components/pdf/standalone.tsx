96d52d33f13aeb6db1ceee1ed58ffe0efe41ae49
Alex Shilen <ashilen1@jh.edu>
2025-08-25 14:33:14 -0400

    feat: use navigation for pdf links if site internal

--- src/components/pdf/standalone.tsx
+++ src/components/pdf/standalone.tsx
@@ -1,10 +1,11 @@
 
 
-import { CSSProperties, forwardRef, useEffect, useState } from "react";
+import { CSSProperties, EventHandler, forwardRef, useEffect, useState } from "react";
 import { Document, Page } from "react-pdf";
 import "./standalone.css";
 import useDimsCtx from "../../context/dims";
 import { useShallow } from "zustand/react/shallow";
+import { useNavigation } from "../../hooks/useNavigation";
 
 export type StandaloneProps = {
   file?: string;
@@ -16,11 +17,12 @@ export type StandaloneProps = {
 const Standalone = forwardRef<HTMLDivElement, StandaloneProps>(({ file, style, path, className = "" }, ref) => {
   const { scale, setMaxPdfWidth } = useDimsCtx(useShallow(({ scale, setMaxPdfWidth }) => ({ scale, setMaxPdfWidth })));
   const [pages, setPages] = useState(0);
+  const { navigate } = useNavigation();
 
   const handlePageRef = (node: HTMLCanvasElement) => {
     if (!node) return;
     setMaxPdfWidth(node.offsetWidth);
-  }
+  };
 
   useEffect(() => {
     if (pages > 1) throw new Error(`Standalone PDF expects a single page document; got ${pages} pages.`);
@@ -28,7 +30,17 @@ const Standalone = forwardRef<HTMLDivElement, StandaloneProps>(({ file, style, p
 
   return <div className={`Standalone ${className}`} style={style} ref={ref} data-path={path}>
     {file && <Document file={`data:application/pdf;base64,${file}`} onLoadSuccess={({ numPages }) => setPages(numPages)}>
-      <Page pageNumber={1} canvasRef={handlePageRef} renderTextLayer={false} scale={scale} />
+      <Page onClick={(e) => {
+        if (!e.target?.href) return;
+        if (typeof e.target.href !== "string") return;
+
+        const url = new URL(e.target.href);
+
+        if (url.hostname !== window.location.hostname) return;
+
+        e.preventDefault();
+        navigate(url.pathname);
+      }} pageNumber={1} canvasRef={handlePageRef} renderTextLayer={false} scale={scale} />
     </Document>}
   </div>
 });
